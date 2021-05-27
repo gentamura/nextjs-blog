f147e40df1cf878c40779c5fd7bbb28fadffe9fa
Gen Tamura <gen.tamura84@gmail.com>
2021-05-27 18:54:53 +0900

    Add absolute imports

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
+const path = require('path');
+
 module.exports = {
   future: {
     webpack5: true,
+  },
+  webpack(config, options) {
+    config.resolve.alias['~'] = path.join(__dirname, 'src');
+
+    return config
   },
 };
