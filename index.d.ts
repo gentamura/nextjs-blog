6c546d33945b7b42cb4a68fa98f4e5d1a833cdaa
Gen Tamura <gen.tamura84@gmail.com>
2022-02-13 16:54:23 +0900

    Update packages and types

---
+++ index.d.ts
@@ -0,0 +1,44 @@
+interface Window {
+  // pageview
+  gtag(
+    type: 'config',
+    googleAnalyticsId: string,
+    {
+      page_path: string,
+    },
+  );
+
+  // event
+  gtag(
+    type: 'event',
+    eventAction: string,
+    fieldObject: {
+      event_label: string,
+      event_category: string,
+      value?: string,
+    },
+  );
+}
+
+interface GaEvent {
+  action: string;
+  category: string;
+  label: string;
+  value?: string;
+}
+
+interface Param {
+  params: {
+    id: string,
+  };
+}
+
+interface PostMeta {
+  id: string;
+  date: string;
+  title: string;
+}
+
+interface Post extends PostMeta {
+  contentHtml: string;
+}
